5862951a615aa0dbc7cadb66300d1be83084c225
Cody Gillespie <cody.gillespie.az@gmail.com>
2023-03-27 00:48:50 -0700

    Tweak publish nuget script
    Fix readme before pushing

--- tools/RunTests.ps1
+++ tools/RunTests.ps1
@@ -2,7 +2,7 @@ $solutionName = "AppFiles.sln"
 $unitTestsCsproj = "AppFiles.Tests\AppFiles.Tests.csproj"
 
 # checks
-Write-Host "Checking that this script is being run from the root of the repository..." -ForegroundColor Green
+Write-Host "Checking that this script is being run from the root of the repository..." -ForegroundColor Gray
 if (!(Test-Path $solutionName)) {
     Write-Host "`tThis script must be run from the root of the repository." -ForegroundColor Red
     exit 1
@@ -10,7 +10,7 @@ if (!(Test-Path $solutionName)) {
 Write-Host "`tOK" -ForegroundColor Green
 
 # run tests
-Write-Host "Running unit tests..." -ForegroundColor Green
+Write-Host "Running unit tests..." -ForegroundColor Gray
 dotnet test $unitTestsCsproj
 if ($LASTEXITCODE -ne 0) {
     Write-Host "`tOne or more unit tests failed." -ForegroundColor Red
